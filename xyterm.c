a44ce621608b71ca2ed85ca8a969675962cbffa0
Bert Münnich <xyb3rt@gmail.com>
2024-09-30 13:04:08 +0200

    Fix handling argv and cwd in xyterm

--- xyterm.c
+++ xyterm.c
@@ -1,8 +1,8 @@
+#include <fcntl.h>
 #include <gtk/gtk.h>
 #include <vte/vte.h>
 
 int acme;
-char *defcmd[3] = {"sh"}, **cmd = defcmd;
 const char *font = "monospace 10";
 const char *colors[18] = {
 	"#2e3436", "#cc0000", "#4e9a06", "#c4a000", "#3465a4", "#75507b",
@@ -14,20 +14,26 @@ static const double zooms[] = {
 	1.0/1.728, 1.0/1.44, 1.0/1.2, 1.0, 1.0*1.2, 1.0*1.44, 1.0*1.728
 };
 
-static void new_window(GApplication *app);
+static void new_window(GApplication *app, char **argv, const char *cwd);
 
-static void on_activate(GApplication *app) {
-	new_window(app);
+static gint on_cmdline(GApplication *app, GApplicationCommandLine *cmdline) {
+	int argc;
+	char **argv = g_application_command_line_get_arguments(cmdline, &argc);
+	const char *cwd = g_application_command_line_get_cwd(cmdline);
+	new_window(app, argc > 1 ? &argv[1] : NULL, cwd);
+	g_strfreev(argv);
+	return 0;
 }
 
 static gboolean on_key(GtkEventController *controller, guint key, guint code,
 		GdkModifierType mod, gpointer data) {
 	GtkWindow *win = (GtkWindow *)data;
+	GApplication *app = G_APPLICATION(gtk_window_get_application(win));
 	VteTerminal *term = VTE_TERMINAL(gtk_window_get_child(win));
 	if (!(mod & GDK_CONTROL_MASK)) {
 		return FALSE;
 	} else if (key == GDK_KEY_N && (mod & GDK_SHIFT_MASK)) {
-		new_window(G_APPLICATION(gtk_window_get_application(win)));
+		new_window(app, NULL, NULL);
 	} else if (key == GDK_KEY_C && (mod & GDK_SHIFT_MASK)) {
 		vte_terminal_copy_clipboard_format(term, VTE_FORMAT_TEXT);
 	} else if (key == GDK_KEY_V && (mod & GDK_SHIFT_MASK)) {
@@ -73,7 +79,7 @@ static void on_exited(VteTerminal *term, int status, gpointer data) {
 	gtk_window_close(win);
 }
 
-static void new_window(GApplication *app) {
+static void new_window(GApplication *app, char **argv, const char *cwd) {
 	GtkWidget *widget;
 	widget = gtk_application_window_new(GTK_APPLICATION(app));
 	GtkWindow *win = GTK_WINDOW(widget);
@@ -95,38 +101,45 @@ static void new_window(GApplication *app) {
 	GtkEventController *controller = gtk_event_controller_key_new();
 	g_signal_connect(controller, "key-pressed", G_CALLBACK(on_key), win);
 	gtk_widget_add_controller(widget, controller);
+	char *cmd[3] = {"sh"};
 	if (acme) {
+		int fd = cwd ? open(cwd, O_RDONLY | O_DIRECTORY) : AT_FDCWD;
+		if (fd != -1) {
+			if (faccessat(fd, "guide", F_OK, 0) == 0) {
+				cmd[1] = "guide";
+			}
+			if (fd != AT_FDCWD) {
+				close(fd);
+			}
+		}
+		cmd[0] = "vim";
+		argv = cmd;
 		vte_terminal_set_size(term, 100, 60);
 		vte_terminal_set_scrollback_lines(term, 0);
+	} else if (argv == NULL) {
+		char *sh = getenv("SHELL");
+		if (sh && sh[0] != '\0') {
+			cmd[0] = sh;
+		}
+		argv = cmd;
 	}
 	gtk_widget_grab_focus(widget);
 	gtk_window_present(win);
-	vte_terminal_spawn_async(term, VTE_PTY_DEFAULT, NULL, cmd, NULL,
+	vte_terminal_spawn_async(term, VTE_PTY_DEFAULT, cwd, argv, NULL,
 			G_SPAWN_DEFAULT, NULL, NULL, NULL, -1, NULL, NULL,
 			NULL);
 }
 
 int main(int argc, char *argv[]) {
 	const char *s = argc > 0 ? strrchr(argv[0], '/') : NULL;
-	if (strcmp(s ? &s[1] : argv[0], "acme") == 0) {
-		acme = 1;
-		defcmd[0] = "vim";
-		defcmd[1] = "guide";
-	} else if (argc > 1) {
-		cmd = &argv[1];
-	} else {
-		char *sh = getenv("SHELL");
-		if (sh != NULL && sh[0] != '\0') {
-			defcmd[0] = sh;
-		}
-	}
+	acme = strcmp(s ? &s[1] : argv[0], "acme") == 0;
 	gtk_init();
 	for (size_t i = 0; i < G_N_ELEMENTS(colors); i++) {
 		gdk_rgba_parse(&palette[i], colors[i]);
 	}
 	g_set_application_name(acme ? "Acme" : "XyTerm");
 	GtkApplication *app = gtk_application_new(acme ? "xyb3rt.acme" :
-			"xyb3rt.xyterm", G_APPLICATION_DEFAULT_FLAGS);
-	g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
-	return g_application_run(G_APPLICATION(app), 0, NULL);
+			"xyb3rt.xyterm", G_APPLICATION_HANDLES_COMMAND_LINE);
+	g_signal_connect(app, "command-line", G_CALLBACK(on_cmdline), NULL);
+	return g_application_run(G_APPLICATION(app), argc, argv);
 }
