88fc9b85f7d23848455e1a36f3b57cfffc8b2d08
Bert Münnich <xyb3rt@gmail.com>
2025-02-25 10:13:27 +0100

    Add x.sh for running scripts in (git) projects

---
+++ x.sh
@@ -0,0 +1,13 @@
+cd "$(dirname "$0")"
+up_env() {
+	while :; do
+		[ -e env.sh ] && set -- "$PWD/env.sh" "$@"
+		[ -d .git ] && break
+		[ "$PWD" = / ] && { echo "$0: not a project" >&2; exit 1; }
+		cd ..
+	done
+	for rc; do
+		. "$rc"
+	done
+}
+up_env
